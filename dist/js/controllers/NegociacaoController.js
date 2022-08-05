b45d76e341cec41cb6a6135ad95cb5ee9d9f7055
João Lourenço <joaodslourenco@gmail.com>
2022-08-05 16:00:35 -0300

    refactor: defining types to negociacao controller and model

---
+++ dist/js/controllers/NegociacaoController.js
@@ -0,0 +1,28 @@
+import { Negociacao } from "../models/negociacao.js";
+class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(date, quantidade, valor);
+        return negociacao;
+    }
+    limparFormulario() {
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
+        this.inputData.focus();
+    }
+}
+export { NegociacaoController };
