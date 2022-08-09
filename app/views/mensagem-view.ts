899f0a9fd4e67ad7c11fb4ead6c5617d1ff3b9fd
João Lourenço <joaodslourenco@gmail.com>
2022-08-09 10:39:13 -0300

    refactor: using heritage to avoid code repetition

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,9 +1,6 @@
-export class MensagemView {
-  private element: HTMLElement;
-  constructor(seletor: string) {
-    this.element = document.querySelector(seletor);
-  }
+import { View } from "./view.js";
 
+export class MensagemView extends View {
   template(model: string): string {
     return `
         <p class="alert alert-info">${model}</p>
@@ -12,6 +9,6 @@ export class MensagemView {
 
   update(model: string): void {
     const template = this.template(model);
-    this.element.innerHTML = template;
+    this.elemento.innerHTML = template;
   }
 }
