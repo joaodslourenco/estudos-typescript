6e887c4cad9b0bd9e827d8c133235edd53c18371
João Lourenço <joaodslourenco@gmail.com>
2022-08-12 11:14:25 -0300

    feat: inspector decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspector } from "../decorators/inspector.js";
 import { logarTempoDeExecucao } from "../decorators/logarTempoDeExecucao.js";
 
 export abstract class View<T> {
@@ -17,6 +18,7 @@ export abstract class View<T> {
   }
   protected abstract template(model: T): string;
 
+  @inspector()
   @logarTempoDeExecucao(true)
   public update(model: T): void {
     let template = this.template(model);
