e88d143bc12e199a35398b465fd76ffeb2669d05
João Lourenço <joaodslourenco@gmail.com>
2022-08-16 16:32:05 -0300

    feat: import is fetching data from api

--- app/dist/js/controllers/NegociacaoController.js
+++ app/dist/js/controllers/NegociacaoController.js
@@ -40,7 +40,19 @@ class NegociacaoController {
         this.mensagemView.update("Negociação adicionada com sucesso.");
     }
     importaDados() {
-        alert("oi");
+        fetch("http://localhost:8080/dados")
+            .then((res) => res.json())
+            .then((dados) => {
+            return dados.map((dadoDeHoje) => {
+                return new Negociacao(new Date(), dadoDeHoje.vezes, dadoDeHoje.montante);
+            });
+        })
+            .then((negociacoesDeHoje) => {
+            for (let negociacao of negociacoesDeHoje) {
+                this.negociacoes.adiciona(negociacao);
+            }
+            this.negociacoesView.update(this.negociacoes);
+        });
     }
     ehDiaUtil(data) {
         return (data.getDay() > DiasDaSemana.DOMINGO &&
--- app/src/controllers/NegociacaoController.ts
+++ app/src/controllers/NegociacaoController.ts
@@ -51,8 +51,24 @@ class NegociacaoController {
     this.mensagemView.update("Negociação adicionada com sucesso.");
   }
 
-  importaDados(): void {
-    alert("oi");
+  public importaDados(): void {
+    fetch("http://localhost:8080/dados")
+      .then((res) => res.json())
+      .then((dados: any[]) => {
+        return dados.map((dadoDeHoje) => {
+          return new Negociacao(
+            new Date(),
+            dadoDeHoje.vezes,
+            dadoDeHoje.montante,
+          );
+        });
+      })
+      .then((negociacoesDeHoje) => {
+        for (let negociacao of negociacoesDeHoje) {
+          this.negociacoes.adiciona(negociacao);
+        }
+        this.negociacoesView.update(this.negociacoes);
+      });
   }
 
   private ehDiaUtil(data: Date) {
