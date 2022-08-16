d709a3f3286dbf120e0990dd36f061d126b5afc3
João Lourenço <joaodslourenco@gmail.com>
2022-08-16 15:37:19 -0300

    feat: domInjector decorator to get dom elements

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -14,8 +14,6 @@ export abstract class View<T> {
   }
   protected abstract template(model: T): string;
 
-  @logarTempoDeExecucao(true)
-  @inspector
   public update(model: T): void {
     let template = this.template(model);
     this.elemento.innerHTML = template;
